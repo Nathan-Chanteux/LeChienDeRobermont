b7aa98dee9e72b5db863bb1fd4b5eeba6ef04fd0
Nathan <nathanchanteux@gmail.com>
2015-09-10 21:44:20 +0200

    Authentification
    Début de la mise en place de l'authentification au Back Office

--- app/Http/Controllers/Articles/ArticlesController.php
+++ app/Http/Controllers/Articles/ArticlesController.php
@@ -10,6 +10,9 @@ use Illuminate\Http\Request;
 
 class ArticlesController extends Controller {
     
+    public function __construct() {
+        $this->middleware('auth');
+    }
     /**
     * Show the form for creating a new resource.
     *
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,61 +1,40 @@
 @extends('app')
 
-@section('content')
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
-				<div class="panel-heading">Login</div>
-				<div class="panel-body">
-					@if (count($errors) > 0)
-						<div class="alert alert-danger">
-							<strong>Whoops!</strong> There were some problems with your input.<br><br>
-							<ul>
-								@foreach ($errors->all() as $error)
-									<li>{{ $error }}</li>
-								@endforeach
-							</ul>
-						</div>
-					@endif
+@section('contenu')
+<section id="contact-page" class="page-systeme page-contenu" style="width: 1516px; min-height: 796px;">
+    <article>
+            <h2 class="text-center transform titreArticle">Login</h2>
+    <div class="article row lato  ">
+        <div class="text-center transform thumb"></div>
+        <div>
+            <div role="form" class="wpcf7" id="wpcf7-f44-o1" lang="fr-FR" dir="ltr">
+            <div class="screen-reader-response"></div>
+                @if (count($errors) > 0)
+                <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                <ul>
+                @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+                </ul>
+                @endif
+                <form role="form" method="POST" action="{{ url('/auth/login') }}">
+                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                    <div><p>E-Mail Address</p>
+                        <input type="email" class="form-control" name="email" value="{{ old('email') }}"></div>
 
-					<form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/login') }}">
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
+                    <div><p>Password</p>
+                        <input type="password" class="form-control" name="password"></div>
+                    <div><label></label>
+                        <input type="checkbox" name="remember"> Remember Me</div>
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">E-Mail Address</label>
-							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
-							</div>
-						</div>
-
-						<div class="form-group">
-							<label class="col-md-4 control-label">Password</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password">
-							</div>
-						</div>
-
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<div class="checkbox">
-									<label>
-										<input type="checkbox" name="remember"> Remember Me
-									</label>
-								</div>
-							</div>
-						</div>
-
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">Login</button>
-
-								<a class="btn btn-link" href="{{ url('/password/email') }}">Forgot Your Password?</a>
-							</div>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+                    <button type="submit">Login</button>
+                </form>
+                <p><a href="{{ URL::asset('/') }}">Page d'accueil</a></p>
+                </div>
+            </div>
+        </div>
+        </article>
+    </div>
+</section>
 @endsection
+        
